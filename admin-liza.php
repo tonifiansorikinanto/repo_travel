00d43e3b8cb4abe042d85349a8633aff0b05caf7
rizkywahyutrisnap <58561649+rizkywahyutrisnap@users.noreply.github.com>
2020-03-06 15:45:26 +0700

    Update admin-liza.php

--- admin-liza.php
+++ admin-liza.php
@@ -393,7 +393,7 @@
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <h5 class="modal-title" id="exampleModalLongTitle">Masukan Keterangan Mobil</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"onclick="resetUrl()">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="resetUrl()">
 	          <span aria-hidden="true">&times;</span>
 	        </button>
 	      </div>
